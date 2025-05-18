2814401d695ceae733e646fdc482e180898e5fc4
Greyrat7490 <kevinschaperjahn13@gmail.com>
2025-05-18 18:33:50 +0200

    graceful shutdown and restart

--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -44,17 +50,7 @@ func usersHandler(c *gin.Context) {
 	})
 }
 
-func main() {
-	debugMode := flag.Bool("d", false, "debug mode")
-    flag.Parse()
-
-	db["user1"] = "passwd"
-	db["user2"] = "passwd2"
-
-	if !*debugMode {
-		gin.SetMode(gin.ReleaseMode)
-	}
-
+func setupServer() *http.Server {
 	router := gin.Default()
 
 	router.SetTrustedProxies([]string{"127.0.0.1/8"})
@@ -67,6 +63,53 @@ func main() {
 	router.GET("/api/ping", pingHandler)
 	router.GET("/api/user/:name", userHandler)
 
+	return &http.Server{
+		Addr:    ":8080",
+		Handler: router.Handler(),
+	}
+}
 
-	router.Run(":8080")
+func startServer(srv *http.Server) {
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+}
+
+func catchShutdown() {
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+}
+
+func gracefullyShutdown(srv *http.Server) {
+	log.Println("Shuting down Server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Println("Server Shutdown error:", err)
+	}
+	<-ctx.Done()
+
+	log.Println("5 seconds timeout reached")
+}
+
+func main() {
+	db["user1"] = "passwd"
+	db["user2"] = "passwd2"
+
+	debugMode := flag.Bool("d", false, "debug mode")
+    flag.Parse()
+	if !*debugMode {
+		gin.SetMode(gin.ReleaseMode)
+	}
+
+	srv := setupServer()
+	startServer(srv)
+	catchShutdown()
+	gracefullyShutdown(srv)
+
+	log.Println("Server shutdown gracefully")
 }
